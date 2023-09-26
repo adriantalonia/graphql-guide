67d6a7048fcd4539076b048fb724822e4f40c55b
Adrian <adrian.talonia11@gmail.com>
2023-09-25 23:58:59 -0600

    add entity user

---
+++ user-registration/src/main/java/com/ur/registration/model/entity/User.java
@@ -0,0 +1,30 @@
+package com.ur.registration.model.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private Boolean active;
+}
