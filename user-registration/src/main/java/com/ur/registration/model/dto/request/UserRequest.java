3e2f5fe9078689a6d81ff88f162975a6ff194b81
Adrian <adrian.talonia11@gmail.com>
2023-10-12 22:28:37 -0600

    add validation dependency

--- user-registration/src/main/java/com/ur/registration/model/dto/request/UserRequest.java
+++ user-registration/src/main/java/com/ur/registration/model/dto/request/UserRequest.java
@@ -1,9 +1,14 @@
 package com.ur.registration.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+
+    @NotBlank
+    @NotNull
     private String firstName;
     private String lastName;
     private String password;
