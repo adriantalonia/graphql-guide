199628a6c152e625669905a128093551ceb84c27
Adrian <adrian.talonia11@gmail.com>
2024-03-07 22:47:46 -0600

    avoid wild card

--- user-registration/src/main/java/com/ur/registration/model/entity/BaseEntity.java
+++ user-registration/src/main/java/com/ur/registration/model/entity/BaseEntity.java
@@ -1,6 +1,11 @@
 package com.ur.registration.model.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
